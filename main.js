2c1364598bfb6c60879fd80e4324d4972dd3e1cf
James Richards <jrichards810@gmail.com>
2018-11-10 15:53:44 -0800

    Update main.js

--- main.js
+++ main.js
@@ -7,24 +7,8 @@ $(document).ready(function(){
                                'slow');    
     })
     
-    function scrollNav() {
-  $('.navbar a').click(function(){  
-    //Toggle Class
-    $(".active").removeClass("actived");      
-    $(this).closest('li').addClass("actived");
-    var theClass = $(this).attr("class");
-    $('.'+theClass).parent('li').addClass('actived');
-    //Animate
-    $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top - 160
-    }, 400);
-    return false;
-  });
-  $('.scrollTop a').scrollTop();
-}
-scrollNav(); 
     //NAV BORDER APPEAR WHEN SCROLL
-    $(window).scroll(function() {
+     $(window).scroll(function() {
           // checks if window is scrolled more than 500px, adds/removes solid class
         if($(this).scrollTop() > 1) { 
             $('.navbar').addClass('solid');
@@ -32,6 +16,24 @@ scrollNav();
             $('.navbar').removeClass('solid');
         }
     })
+    
+    //NAV BAR BUTTON SCROLL
+    function scrollNav() {
+        $('.navbar a').click(function(){  
+            //Toggle Class
+            $(".actived").removeClass("actived");      
+            $(this).closest('li').addClass("actived");
+            var theClass = $(this).attr("class");
+            $('.'+theClass).parent('li').addClass('actived');
+            //Animate
+            $('html, body').stop().animate({
+                scrollTop: $( $(this).attr('href') ).offset().top - 160
+            }, 400);
+            return false;
+        });
+//  $('.scrollTop a').scrollTop();
+    }
+    scrollNav(); 
 
 });
     
