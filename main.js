4b9f02354f56f55455f71d711d82e0fd3d892098
James Richards <jrichards810@gmail.com>
2018-11-09 18:33:10 -0800

    Update main.js

--- main.js
+++ main.js
@@ -7,26 +7,22 @@ $(document).ready(function(){
                                'slow');    
     })
     
-    //ABOUT BUTTON SCROLL
-    $(".abt").click(function() {
-        $('html,body').animate({
-            scrollTop: $("#about").offset().top},
-                               'slow');
-        })
-    //PROJECTS BUTTON SCROLL
-    $(".prjcts").click(function() {
-        $('html,body').animate({
-            scrollTop: $("#projects").offset().top},
-                               'slow');
-    })
-    
-    //CONTACT BUTTON SCROLL
-    $(".cntct").click(function() {
-        $('html,body').animate({
-            scrollTop: $("#contact").offset().top},
-                               'slow');
-    })
-    
+    function scrollNav() {
+  $('.navbar a').click(function(){  
+    //Toggle Class
+    $(".active").removeClass("active");      
+    $(this).closest('li').addClass("active");
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('li').addClass('active');
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 160
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav(); 
     //NAV BORDER APPEAR WHEN SCROLL
     $(window).scroll(function() {
           // checks if window is scrolled more than 500px, adds/removes solid class
