7e6b9246a28d2d933e5fc9ab149a58bc7e482f95
James Richards <jrichards810@gmail.com>
2018-11-09 16:35:12 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 $(document).ready(function(){
-   $(".glyphicon").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#about").offset().top},
-        'slow');    
-});
-  $(window).scroll(function() {
-          // checks if window is scrolled more than 500px, adds/removes solid class
-          if($(this).scrollTop() > 1) { 
-              $('.navbar').addClass('solid');
-          } else {
-              $('.navbar').removeClass('solid');
-          }
-        });
+
+    //ARROW SCROLL
+    $(".glyphicon").click(function() {
+        $('html,body').animate({
+            scrollTop: $("#about").offset().top},
+                               'slow');    
+    })
     
+    //ABOUT BUTTON SCROLL
+    $(".abt").click(function() {
+        $('html,body').animate({
+            scrollTop: $("#about").offset().top},
+                               'slow');
+        })
+    //PROJECTS BUTTON SCROLL
+    $(".prjcts").click(function() {
+        $('html,body').animate({
+            scrollTop: $("#projects").offset().top},
+                               'slow');
+    })
+    
+    //CONTACT BUTTON SCROLL
+    $(".cntct").click(function() {
+        $('html,body').animate({
+            scrollTop: $("#contact").offset().top},
+                               'slow');
+    })
+    
+    //NAV BORDER APPEAR WHEN SCROLL
+    $(window).scroll(function() {
+          // checks if window is scrolled more than 500px, adds/removes solid class
+        if($(this).scrollTop() > 1) { 
+            $('.navbar').addClass('solid');
+        } else {
+            $('.navbar').removeClass('solid');
+        }
+    })
+
 });
+    
